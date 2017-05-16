99b2750a64534db51b5bafffbbf2a085e0e80337
X <x@gmail.com>
2017-05-16 22:44:19 +0300

    adding common components

---
+++ src/components/common/Header.js
@@ -0,0 +1,36 @@
+// Import libraris for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make a component
+// which receive props sent from parent component ( app )
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{ props.headerText }</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, hegith: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+// Make the component available to other parts of the app
+export { Header };
---
+++ src/components/common/Spinner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+const Spinner = ({ spinnerSize }) => {
+  return (
+    <View style={styles.spinnerStyle} >
+      <ActivityIndicator size={spinnerSize || 'large'} />
+    </View>
+  );
+};
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
+
+export { Spinner };
