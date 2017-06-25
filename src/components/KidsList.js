6ea25b1f8311b1f9bc623c6aa5d5dda93e19302e
thefrieddan1 <thefrieddan@gmail.com>
2017-06-25 11:04:32 +0300

    added functionality to search header

--- src/components/KidsList.js
+++ src/components/KidsList.js
@@ -1,12 +1,28 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { ListView, View, StyleSheet } from 'react-native';
+import { ListView, TextInput, View, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { kidFetch, kidsFetch } from '../actions';
 import ListItem from './ListItem';
 import { SearchHeader, SectionHeader } from './common';
 
 class KidsList extends Component {
+  constructor(props) {
+    super(props);
+    const getSectionData = (dataBlob, sectionId) => dataBlob[sectionId];
+    const getRowData = (dataBlob, sectionId, rowId) => dataBlob[`${rowId}`];
+
+    this.ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+        getSectionData,
+        getRowData
+    });
+    this.state = {
+      dataSource: this.createDataSource(this.props)
+    };
+  }
+
   componentWillMount() {
     const id = this.props.data;
     // id is not null when parent log in.
@@ -15,26 +31,20 @@ class KidsList extends Component {
     } else {
       this.props.kidsFetch();
     }
-    this.createDataSource(this.props);
   }
 
   componentWillReceiveProps(nextProps) {
-    this.createDataSource(nextProps);
+    this.setDataSource(nextProps);
+  }
+
+  setDataSource({ babies }) {
+    const { dataBlob, sectionIds, rowIds } = this.formatData(babies);
+    this.setState({ dataSource: this.ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds) });
   }
 
   createDataSource({ babies }) {
-    const getSectionData = (dataBlob, sectionId) => dataBlob[sectionId];
-    const getRowData = (dataBlob, sectionId, rowId) => dataBlob[`${rowId}`];
-
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
-        getSectionData,
-        getRowData
-    });
-
     const { dataBlob, sectionIds, rowIds } = this.formatData(babies);
-    this.dataSource = ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds);
+    return this.ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds);
   }
 
   /*
@@ -72,15 +82,39 @@ class KidsList extends Component {
     return <ListItem baby={baby} />;
   }
 
+  renderSearchHeader({ babies }) {
+    return (
+    <View style={styles.searchContainer}>
+      <TextInput
+        style={styles.input}
+        placeholder="Search..."
+        onChangeText={(text) => {
+          const rows = [];
+          rows.babies = [];
+          for (let i = 0; i < babies.length; i++) {
+            const firstName = babies[i].name.toLowerCase();
+            const lastName = babies[i].lastName.toLowerCase();
+            if (firstName.search(text.toLowerCase()) !== -1 ||
+                lastName.search(text.toLowerCase()) !== -1) {
+              rows.babies.push(babies[i]);
+            }
+          }
+          this.setDataSource(rows);
+        }}
+      />
+    </View>
+  );
+}
+
   render() {
     return (
         <ListView
           style={styles.container}
           enableEmptySections
-          dataSource={this.dataSource}
+          dataSource={this.state.dataSource}
           renderRow={this.renderRow}
           renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator} />}
-          renderHeader={() => <SearchHeader />}
+          renderHeader={() => this.renderSearchHeader(this.props)}
           renderSectionHeader={(sectionData) => <SectionHeader {...sectionData} />}
         />
     );
@@ -96,6 +130,21 @@ const styles = StyleSheet.create({
     height: StyleSheet.hairlineWidth,
     backgroundColor: '#8E8E8E',
   },
+  searchContainer: {
+    flex: 1,
+    padding: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#C1C1C1',
+  },
+  input: {
+    height: 30,
+    flex: 1,
+    paddingHorizontal: 8,
+    fontSize: 15,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 2,
+  }
 });
 
 const mapStateToProps = state => {
